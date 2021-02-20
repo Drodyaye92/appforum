2acc7ce4fdaa9694f24eafb4dddcc1594c858f85
Drodyaye92 <aichanana24@gmail.com>
2021-02-20 20:08:00 +0000

    commit du 20fev

--- dashtraitement.php
+++ dashtraitement.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORUM</title>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="dash.css">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js">
 </head>

a94002060b6ef46e82ef967f099422eec598f168
Drodyaye92 <aichanana24@gmail.com>
2021-02-21 00:54:59 +0000

    commit publication

---
+++ publication.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>publication</title>
+    <link rel="stylesheet" href="styles/home.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/js/bootstrap.min.js">
+</head>
+<body>
+
+section class="container-fluid">
+   <section class="row">
+       <section class=" col-lg-4 col-md-4 col-mx-12 gauche">
+           <div class="cote"> 
+            <div>
+               <img src="images/lacsoftforum.png" alt="" width="350px">
+            </div> 
+        
+         <div action="" method="POST">
+         <div class="form">
+            <div class="tab-tete">
+               <div class="active"><button>See profile</button></div>
+               <div><button> FORUM</button> </div>
+               <div><button> Dashbord</button> </div>
+               <div><button> Register</button> </div>
+            </div>
+         </div> 
+         </div>  
+  
+           </div>
+        </section> 
+<section class="col-lg-8 col-md-8 col-sm-12">
+<div class="login centre_forum">
+        <h3 class="text-uppercase font-weight-bold">Registration</h3>
+       <table>
+          <form action="" method="post">
+          <tr>
+          <td> <input type="text" name="firstname" placeholder="firstname" required="required" class="font-weight-bold"/></td>
+          </tr>
+           
+            <tr>
+          <td> <input type="text" name="lastname" placeholder="lastname" required="required" class="font-weight-bold"/>
+            </td>
+          </tr>
+           <tr>
+          <td><input type="email" name="email" placeholder="email" required="required"  class="font-weight-bold"/></td>
+          </tr>
+            
+            <tr>
+          <td><input type="password" name="mdp" placeholder="password" required="required" class="font-weight-bold"/></td>
+          </tr>
+          <tr>
+          <td><input type="password" name="conf_mdp" placeholder="password" required="required"  class="font-weight-bold"/></td>
+          </tr>
+          <tr>
+          <td><input type="password" name="role" placeholder="role" required="required"  class="font-weight-bold"/></td>
+          </tr>
+            
+            <tr>
+          <td><button type="submit" class="btn btn-primary btn-block btn-large">S'inscrire</button></td>
+          </tr>
+            
+          </form>
+        </table>
+    </div>
+
+    <div> 
+   <table>
+      
+       <form>
+       <tr>
+       <td>
+           <label for=""> email</label>
+       </td>
+          <td><input type="" name="email" placeholder="" required="required"  class="font-weight-bold"/></td>
+        </tr>
+         
+       <tr>
+       <td>
+          <label for=""> message</label>
+       </td>
+          <td> <textarea type="text" name="" placeholder="message" required="required"  class="font-weight-bold"> </textarea></td>
+       </tr>
+            
+         <tr>
+          <td><button type="submit" class="btn btn-primary btn-block btn-large">Send</button></td>
+         </tr>
+         </form>
+      
+   </table>
+    </div>
+
+    </section>
+</body>
+</html>
+ 
